5601ab961e3053075855e280dc80335aa314c129
singhwong <singhwongwxg@hotmail.com>
2021-05-31 20:41:06 +0800

    first commit

---
+++ 短语列表约束/MainPage.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media.SpeechRecognition;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace 短语列表约束
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private SpeechRecognizer _recognizer;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.Loaded += Page_Loaded;
+            this.Unloaded += Page_UnLoaded;
+        }
+
+        private async void Button_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            try
+            {
+                while (true)
+                {
+                    SpeechRecognitionResult result = await _recognizer.RecognizeAsync();
+                    if (result.Status == SpeechRecognitionResultStatus.Success)
+                    {
+                        //处理识别结果
+                        listBox.SelectedItem = result.Text;
+                        if (result.Text == "结束")
+                        {
+                            return;
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+        private async void Page_Loaded(object sender,object e)
+        {
+            _recognizer = new SpeechRecognizer();
+            //创建自定义短语约束
+            string[] arrgy = { "足球", "排球", "跑步", "羽毛球", "橄榄球","结束" };
+            SpeechRecognitionListConstraint listConstraint = new SpeechRecognitionListConstraint(arrgy);
+            //添加约束实例到集合中
+            _recognizer.Constraints.Add(listConstraint);
+            //编译约束
+            await _recognizer.CompileConstraintsAsync();
+        }
+
+        private void Page_UnLoaded(object sender, RoutedEventArgs e)
+        {
+            //释放资源
+            _recognizer.Dispose();
+        }
+    }
+}
---
+++ 自定义语音识别规则/MainPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media.SpeechRecognition;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace 自定义语音识别规则
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void tbn_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            btn.IsEnabled = false;
+            using (SpeechRecognizer recognizer = new SpeechRecognizer())
+            {
+                try
+                {
+                    //加载语法文件
+                    var sgrsFile = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///srgs.xml"));
+                    //创建识别约束
+                    SpeechRecognitionGrammarFileConstraint grammarFileConstraint = new SpeechRecognitionGrammarFileConstraint(sgrsFile);
+                    //加入到约束集合中
+                    recognizer.Constraints.Add(grammarFileConstraint);
+                    //编译约束
+                    SpeechRecognitionCompilationResult compilationResult = await recognizer.CompileConstraintsAsync();
+                    if (compilationResult.Status == SpeechRecognitionResultStatus.Success)
+                    {
+                        //开始识别
+                        SpeechRecognitionResult result = await recognizer.RecognizeAsync();
+                        if (result.Status == SpeechRecognitionResultStatus.Success)
+                        {
+                            tbRes.Text = $"识别结果：{result.Text}";
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                }
+            }
+            btn.IsEnabled = true;
+        }
+    }
+}
